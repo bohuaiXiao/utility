12c1e51191b34834a6e0d01fdc9a13803935fff8
bohuaixiao <tommy19900218@gmail.com>
2020-03-11 20:14:57 -0400

    sa

---
+++ src/day1/Colors.java
@@ -0,0 +1,14 @@
+package day1;
+
+/**
+ * @author:
+ * @create:
+ * @date:
+ */
+public class Colors {
+    public static void main(String[] args) {
+        String[] colors = {"red", "ble","green"};
+
+
+    }
+}
